d9764281ec41db2b64f03255a2244e724668fece
IvanScaramouche <ivan.skaram@ya.ru>
2020-04-05 16:22:16 +0700

    Add SlidingActor

---
+++ Assets/Foundation/Skripts/Actor/SurfaceActor/SlidingActor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scaramouche.Game {
+    public class SlidingActor : Actor {
+
+        private void Start() {
+            ThisTransform = GetComponent<Transform>();
+        }
+    }
+}
